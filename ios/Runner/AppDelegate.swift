5af5c1f96bfb921ed45537c4d79076c1acfef967
Shreenay-Lore <shrinaylore66@gmail.com>
2025-11-10 01:12:07 +0530

    permission changes

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyBccRcY_D5rBgtGOcvYwtpOXLJp2bvR7v0")
+    GMSServices.provideAPIKey("")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
